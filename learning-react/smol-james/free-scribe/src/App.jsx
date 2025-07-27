75b5b79c15bd9e4ce3bd4bd709ede2d83eb5fe0c
Rein <reinX244@gmail.com>
2025-07-28 01:50:48 +0800

    Finished whisper.worker.js code, to be tested.

--- learning-react/smol-james/free-scribe/src/App.jsx
+++ learning-react/smol-james/free-scribe/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import Information from "./components/Information";
 import Transcribing from "./components/Transcribing";
+import { MessageTypes } from "./utils/presets";
 
 function App() {
   const [file, setFile] = useState(null);
@@ -38,17 +39,49 @@ function App() {
           console.log("DOWNLOADING");
           break;
         case "LOADING":
-          console.log("DOWNLOADING");
+          console.log("LOADING");
           break;
         case "RESULT":
-          console.log("DOWNLOADING");
+          console.log("RESULT");
+          setOutput(e.data.results);
           break;
         case "INFERENCE_DONE":
-          console.log("DOWNLOADING");
+          console.log("INFERENCE_DONE");
+          setFinished(true);
           break;
       }
     };
+
+    worker.current.addEventListener("message", onMessageReceived);
+
+    return () =>
+      worker.current.removeEventListener("message", onMessageReceived);
   }, []);
+
+  const readAudioFrom = async (file) => {
+    const sampling_rate = 16000;
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate });
+    const response = await file.arrayBuffer();
+    const decoded = await audioCTX.decodeAudioData(response);
+    const audio = decoded.getChannelData(0);
+
+    return audio;
+  };
+
+  const handleFormSubmission = async () => {
+    if (!file && !audioStream) {
+      return;
+    }
+
+    let audio = await readAudioFrom(file ? file : audioStream);
+    const model_name = `openai/whisper-tiny.en`;
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name,
+    });
+  };
 
   return (
     <div className="bg-gradient-to-r from-blue-50 to-transparent text-slate-700 text-sm sm:text-base">
